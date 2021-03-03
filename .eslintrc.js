bc844c913c82afb79145d6aeedb4283abaccf16d
Cristian <contato@cristiancunha.me>
2021-03-03 16:27:54 -0300

    Feature: inicial structs

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    // "global-require":0,
+    // "indent": [2, 2],
+    'no-nested-ternary': 0,
+    // "max-len": [2, {"code": 140, "tabWidth": 4, "ignoreUrls": true}],
+    'no-param-reassign': 0,
+    'react/forbid-prop-types': 0,
+    'no-underscore-dangle': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-console': ['error', { allow: ['tron'] }],
+    'no-case-declarations': 0,
+    'react/no-array-index-key': 0,
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+};
