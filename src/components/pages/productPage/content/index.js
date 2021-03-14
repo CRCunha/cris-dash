c053caa62df407239c6e73913b49766d6cc526e2
Cristian <contato@cristiancunha.me>
2021-03-14 20:50:12 -0300

    feature: add redux storage products into  dashboard

--- src/components/pages/productPage/content/index.js
+++ src/components/pages/productPage/content/index.js
@@ -1,10 +1,34 @@
-import { React } from 'react';
+import { React, useEffect } from 'react';
 import { Grid } from '@material-ui/core';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import * as ProductActions from '../../../../store/modules/products/actions';
 import ExplanatoryCard from '../../../explanatoryCard';
 import useStyles from './styles';
 
 export default function Content() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const productsStates = useSelector((state) => state.products, []);
+
+  const getAllProducts = () => {
+    axios
+      .get('    https://fakestoreapi.com/products')
+      .then((response) => {
+        dispatch(ProductActions.setProductList(response.data));
+        // eslint-disable-next-line no-console
+        console.log('productsStates', productsStates.products);
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <div>
       <Grid continer>
---
+++ src/store/modules/products/actions.js
@@ -0,0 +1,4 @@
+/* Set Product List */
+export const setProductList = (productsObject) => {
+  return { type: '@app/SET_PRODUCT_LIST', productsObject };
+};
