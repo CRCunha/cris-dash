ac56f149bf95bef261ff6c0d7fa41d1d08625845
Cristian <contato@cristiancunha.me>
2021-03-14 21:27:17 -0300

    fix: get data sku's

--- src/components/pages/productPage/content/index.js
+++ src/components/pages/productPage/content/index.js
@@ -1,33 +1,15 @@
-import { React, useEffect } from 'react';
+import { React } from 'react';
 import { Grid } from '@material-ui/core';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import * as ProductActions from '../../../../store/modules/products/actions';
+import { useSelector } from 'react-redux';
 import ExplanatoryCard from '../../../explanatoryCard';
 import useStyles from './styles';
 
 export default function Content() {
   const classes = useStyles();
-  const dispatch = useDispatch();
+
   const productsStates = useSelector((state) => state.products, []);
-
-  const getAllProducts = () => {
-    axios
-      .get('    https://fakestoreapi.com/products')
-      .then((response) => {
-        dispatch(ProductActions.setProductList(response.data));
-        // eslint-disable-next-line no-console
-        console.log('productsStates', productsStates.products);
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    getAllProducts();
-  }, []);
+  // eslint-disable-next-line no-console
+  console.log('productsStates', productsStates.products);
 
   return (
     <div>
