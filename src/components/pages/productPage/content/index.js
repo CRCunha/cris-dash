14902da4d9a190db6b8d2af1d7d6ee5822bbbb24
Cristian <contato@cristiancunha.me>
2021-03-15 14:11:17 -0300

    feature: cards product styles

--- src/components/pages/productPage/content/index.js
+++ src/components/pages/productPage/content/index.js
@@ -1,6 +1,7 @@
 import { React } from 'react';
 import { Grid } from '@material-ui/core';
 import { useSelector } from 'react-redux';
+import clsx from 'clsx';
 import ExplanatoryCard from '../../../explanatoryCard';
 import useStyles from './styles';
 
@@ -15,9 +16,47 @@ export default function Content() {
     <div>
       <Grid continer>
         <Grid item xs={12}>
-          <Grid className={classes.cardContainer} container justify="center">
+          <Grid container justify="center">
             <Grid item xs={10}>
               <ExplanatoryCard />
+              <Grid
+                container
+                style={{
+                  marginTop: 20,
+                  overflow: 'auto',
+                  height: '50vh',
+                }}
+              >
+                {productsStates.products.map((value) => {
+                  return (
+                    <Grid item xs={12}>
+                      <Grid
+                        container
+                        alignItems="center"
+                        className={clsx(classes.drawer, {
+                          [classes.gridCardError]: value.price < 50.0,
+                          [classes.gridCardWarning]:
+                            value.price >= 50.0 && value.price < 100.0,
+                          [classes.gridCardOk]: value.price > 100.0,
+                        })}
+                      >
+                        <Grid item xs={4}>
+                          {value.title}
+                        </Grid>
+                        <Grid item xs={1}>
+                          {value.price}
+                        </Grid>
+                        <Grid item xs={1}>
+                          {value.category}
+                        </Grid>
+                        <Grid item xs={6} style={{ paddingLeft: 10 }}>
+                          {value.description}
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  );
+                })}
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
